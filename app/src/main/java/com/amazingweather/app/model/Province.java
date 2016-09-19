6a8f4c119add6769b8f09eaf81d25aedc6ff418c
itlizhao <itlizhao@qq.com>
2016-09-19 11:52:47 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/amazingweather/app/model/Province.java
@@ -0,0 +1,34 @@
+package com.amazingweather.app.model;
+
+/**
+ * Created by lee on 16/9/18.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
